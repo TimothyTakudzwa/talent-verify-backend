9282dc715a68ef9a7c004c4f3e75213c0c4cc65b
Timothy Ngorima <timothytakudzwa@gmail.com>
2023-07-20 20:17:13 +0200

    Add TokenObtainPairView and TokenRefreshView

--- talentVerify/settings.py
+++ talentVerify/settings.py
@@ -65,9 +65,13 @@ ROOT_URLCONF = 'talentVerify.urls'
 REST_FRAMEWORK = {
     # Use Django's standard `django.contrib.auth` permissions,
     # or allow read-only access for unauthenticated users.
-    'DEFAULT_PERMISSION_CLASSES': [
+    'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
-    ]
+    ),
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
 }
 
 TEMPLATES = [
